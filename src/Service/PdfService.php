3176266e81bd41557e37df5eb9fc23757f922680
oumayma159 <oumaima.elfidha@insat.ucar.tn>
2023-05-08 13:18:35 +0200

    last changes from the merge

--- src/Service/PdfService.php
+++ src/Service/PdfService.php
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace App\Service ;
-
-class PdfService{
-    private $domPdf ;
-
-    public function __construct(){
-        $this->domPdf = new DomPdf();
-
-        $pdfOption = new Options();
-
-        $pdfOption->set('defaultFont','Garamond');
-
-        $this->domPdf-> setOptions($pdfOption);
-    }
-
-    public function showpdfFile($html)
-    {
-        $this->domPdf->load($html);
-        $this->domPdf->render();
-        $this->domPdf->stream("Details.pdf",['Attachement'=>false]); 
-    }
-
-    
-=======
 <?php 
 
 namespace App\Service;
@@ -68,5 +41,4 @@ class PdfService
 
     $this->domPdf->output();
   }
->>>>>>> main
 }
